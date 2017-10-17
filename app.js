cf0e49fac47b0bd4b1cdde69cb758e7fcce5a6ae
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-10-17 21:28:17 +0300

    added express and ejs

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+// const passportSetup = require('./config/passport-setup');
+
+// import routes
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+
+// set up ejs view
+app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
+
+// homepage route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log('app is listening on port 3000');
+});
---
+++ routes/auth-routes.js
@@ -0,0 +1,10 @@
+// instance of a Router
+const router = require('express').Router();
+// const passport = require('passport');
+
+// auth login
+// router.get('/login', (req, res) => {
+//   res.render('login');
+// });
+
+module.exports = router;
