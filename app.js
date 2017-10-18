93db3b6dba40e464d9a99a972f410d377010c08b
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-10-18 12:20:06 +0300

    modified app.js

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const app = express();
 var params = {
   UserName: 'Bob'
 };
+
 iam.createUser(params, function(err, data){
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
